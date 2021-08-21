ecdfec842a492fe0187fed874add3ac416632e4e
Kytra Murphree <kytrascript@gmail.com>
2021-08-21 00:44:04 -0500

    Some updated styles. Testing sidebar feasibility.

--- pages/index.js
+++ pages/index.js
@@ -23,37 +23,11 @@ function Home({router}) {
             <>
                 <Navigation routerAction={pushRouter}/>
                 <Container className="board-container" fluid>
-                    <Row>
-                        <Col>1 / 3</Col>
+                    <Row className={"content-row"}>
+                        <Col className={"left-sidebar"}>
+
+                        </Col>
                         <Col className={"board-column"}>
-                            {/*<div className={'input-wrapper'}>*/}
-                            {/*    <div className={'vert'}>*/}
-                            {/*        <div className={'input-col'}>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*        </div>*/}
-                            {/*        <div className={'input-col'}>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*            <input className={'rule'} maxLength={2}/>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
                             {router.query.board === '5' ? 'THIS WOULD BE A 5 x 5' : ''}
                             {router.query.board === '10' ? <TenGrid/> : ''}
                             {router.query.board === '15' ? 'THIS WOULD BE A 15 x 15' : ''}
